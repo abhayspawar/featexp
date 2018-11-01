4d42abde14a131c11a9d6dcc8a0b69b8d5d805f5
abhayspawar <abhayspawar@gmail.com>
2018-11-01 10:55:26 -0700

    added leaky feature image

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="featexp",
-    version="0.0.22",
+    version="0.0.3",
     author="Abhay Pawar",
     author_email="abhayspawar@gmail.com",
     description="Feature exploration for supervised learning",
