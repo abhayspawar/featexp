e57a9f548f467917265a7bf508dc0f1cff637e26
Abhay Pawar <abhay.pawar@stitchfix.com>
2020-07-25 21:46:31 -0700

    next release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="featexp",
-    version="0.0.6",
+    version="0.0.7",
     author="Abhay Pawar",
     author_email="abhayspawar@gmail.com",
     description="Feature exploration for supervised learning",
